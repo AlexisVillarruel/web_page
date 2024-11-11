86517f0c12c25b8d813c73fddb1ac254165fb1fb
AlexisVillarruel <alexisvillarruelj14@gmail.com>
2024-11-11 16:17:34 -0500

    WEB PAGE NEXFLOW

---
+++ script.js
@@ -0,0 +1,9 @@
+// Desplazamiento suave para los enlaces de navegación
+document.querySelectorAll('.nav a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
